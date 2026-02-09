6089c89461a38fe39a51ac4ff04956e3b2e1cdfd
bitmatrix <22298248+NVRC@users.noreply.github.com>
2026-02-08 19:18:37 -0500

    feat(cli): expose `l-system` main

--- src/l_system/__main__.py
+++ src/l_system/__main__.py
@@ -1,5 +1,10 @@
 from l_system.rendering.renderer import LSystemRenderer
 
-if __name__ == '__main__':
+
+def main() -> None:
     renderer = LSystemRenderer()
     renderer.draw()
+
+
+if __name__ == '__main__':
+    main()
